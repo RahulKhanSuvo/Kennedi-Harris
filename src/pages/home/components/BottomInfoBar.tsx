53f04d1e749913c686e7f0c039bd4ff52f344081
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-21 10:21:03 +0600

    refactor: update basketball icon and increase font-size for BottomInfoBar icons

--- src/pages/home/components/BottomInfoBar.tsx
+++ src/pages/home/components/BottomInfoBar.tsx
@@ -1,6 +1,6 @@
 import { MapPin, Star } from "lucide-react";
 import { LuCalendarDays } from "react-icons/lu";
-import { PiBasketballLight } from "react-icons/pi";
+import { PiBasketball } from "react-icons/pi";
 import { TbRuler2 } from "react-icons/tb";
 
 export default function BottomInfoBar() {
@@ -8,25 +8,25 @@ export default function BottomInfoBar() {
     <div className="w-full border-y border-white/10 backdrop-blur mt-auto">
       <div className="max-w-[1920px] w-full mx-auto px-10   flex flex-wrap lg:flex-nowrap items-center justify-between font-condensed font-semibold tracking-widest py-8 text-xl text-white">
         <div className="flex items-center gap-3 stat-bar-item lg:w-1/5 justify-center lg:justify-start">
-          <LuCalendarDays className="text-kh-pink" size={24} />
+          <LuCalendarDays className="text-kh-pink" size={30} />
           CLASS OF 2030
         </div>
         <div className="flex items-center gap-3 stat-bar-item lg:w-1/5 justify-center">
-          {/* <Ruler className="text-kh-pink" size={24} /> */}
-          <TbRuler2 className="text-kh-pink" size={24} />
+          {/* <Ruler className="text-kh-pink" size={30} /> */}
+          <TbRuler2 className="text-kh-pink" size={30} />
           6'2
         </div>
         <div className="flex items-center gap-3 stat-bar-item lg:w-1/5 justify-center">
-          {/* <User className="text-kh-pink" size={24} /> */}
-          <PiBasketballLight className="text-kh-pink" size={24} />
+          {/* <User className="text-kh-pink" size={30} /> */}
+          <PiBasketball className="text-kh-pink" size={30} />
           GUARD / FORWARD
         </div>
         <div className="flex items-center gap-3 stat-bar-item lg:w-1/5 justify-center">
-          <MapPin className="text-kh-pink" size={24} />
+          <MapPin className="text-kh-pink" size={30} />
           GEORGIA
         </div>
         <div className="flex items-center gap-3 stat-bar-item lg:w-1/5 justify-center lg:justify-end">
-          <Star className="text-kh-pink" size={24} />
+          <Star className="text-kh-pink" size={30} />
           NATIONAL PROSPECT
         </div>
       </div>
