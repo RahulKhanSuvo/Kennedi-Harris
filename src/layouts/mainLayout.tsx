612d9f3d36304a2b76d99459ce93f6edda166de2
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-21 06:22:44 +0600

    feat: wrap Outlet in main tag and add footer to mainLayout

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from "react-router";
 
 export default function mainLayout() {
-    return (
-        <>
-            <Outlet />
-        </>
-    )
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <p>All rights reserved &copy; 2026</p>
+      </footer>
+    </>
+  );
 }
